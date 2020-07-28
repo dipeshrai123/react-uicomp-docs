b7282d8357fb11fde55d4d7932ea53906b018506
Aseem Regmi <aseem.regmi22@gmail.com>
2020-07-28 16:08:07 +0545

    Header

--- src/components/common/header/Header.common.js
+++ src/components/common/header/Header.common.js
@@ -2,8 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="header-container">
-      <div className="header">HEADER</div>
+    <div className="header">
+      <nav className="header__wrapper">
+        <h2 className="header__title">React UI Comp</h2>
+      </nav>
     </div>
   );
 };

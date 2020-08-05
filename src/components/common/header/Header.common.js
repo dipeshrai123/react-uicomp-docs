58d1689a98094d100f09440cafb6893521f4f1a7
raidipesh78 <raidipesh78@gmail.com>
2020-08-05 12:25:06 +0545

    Logo added|

--- src/components/common/header/Header.common.js
+++ src/components/common/header/Header.common.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Logo from "../../../assets/icons/Logo.png";
 
 const Header = () => {
   return (
     <div className="header">
       <nav className="header__wrapper">
+        <img src={Logo} alt="React UI Comp Logo" />
         <h2 className="header__title">React UI Comp</h2>
       </nav>
     </div>

bea68a5822e41bb94d52cc7e43fa0284caf2c707
raidipesh78 <raidipesh78@gmail.com>
2020-08-07 19:58:56 +0545

    Table component added

--- src/components/dropdownPage/DropdownPage.js
+++ src/components/dropdownPage/DropdownPage.js
@@ -13,6 +13,40 @@ const DropdownPage = () => {
       <section>
         <Paragraph>Comming soon</Paragraph>
       </section>
+
+      <section>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title 1</th>
+              <th>Title 2</th>
+              <th>Title 3</th>
+              <th>Title 4</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Desc one</td>
+              <td>Desc Two</td>
+              <td>Desc Three</td>
+              <td>Desc Four</td>
+            </tr>
+            <tr>
+              <td>Desc one</td>
+              <td>Desc Two</td>
+              <td>Desc Three</td>
+              <td>Desc Four</td>
+            </tr>
+            <tr>
+              <td>Desc one</td>
+              <td>Desc Two</td>
+              <td>Desc Three</td>
+              <td>Desc Four</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
     </div>
   );
 };

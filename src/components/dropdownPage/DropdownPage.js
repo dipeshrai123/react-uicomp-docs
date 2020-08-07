bd92f9c1254bb8081de9584d07214ea45c66b45a
raidipesh78 <raidipesh78@gmail.com>
2020-08-07 21:09:13 +0545

    :rocket: Dropdown complete

--- src/components/dropdownPage/DropdownPage.js
+++ src/components/dropdownPage/DropdownPage.js
@@ -1,4 +1,7 @@
 import React, { useEffect } from "react";
+import { Dropdown } from "react-uicomp";
+import SecondaryTitle from "../common/secondaryTitle/SecondaryTitle";
+import Code from "../common/code/Code.common";
 import Title from "../common/title/Title";
 import Paragraph from "../common/paragraph/Paragraph";
 
@@ -11,38 +14,149 @@ const DropdownPage = () => {
     <div>
       <Title>Dropdown</Title>
       <section>
-        <Paragraph>Comming soon</Paragraph>
+        <Paragraph>
+          When it comes to creating a dropdown component we should also think of
+          small things such as dismiss the dropdown when clicked outside, some
+          small animations and position of a dropdown. It requires some work to
+          handle all those small details.{" "}
+        </Paragraph>
+        <Paragraph>
+          React UI Comp comes with a{" "}
+          <span className="highlight">&lt;Dropdown&gt;</span> component which
+          handles all those things for you. It converts any component into a
+          dropdown component very easily.
+        </Paragraph>
       </section>
 
       <section>
+        <SecondaryTitle>Demo</SecondaryTitle>
+        <Dropdown triggerElement={() => <button>Toggle Menu</button>}>
+          <div
+            style={{
+              background: "#FFF",
+              padding: 10,
+              border: "1px solid #F1F1F1",
+              boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
+            }}
+          >
+            I am Dropdown Content
+          </div>
+        </Dropdown>
+      </section>
+
+      <section>
+        <SecondaryTitle>Example</SecondaryTitle>
+
+        <Paragraph>
+          You can create any component a trigger element i.e. element which
+          toggles the dropdown.{" "}
+          <span className="highlight">triggerElement</span> prop accepts a
+          function which handles triggering of the dropdown and children inside{" "}
+          <span className="highlight">&lt;Dropdown&gt;</span> component is the
+          content which gets toggled.
+          <Code>
+            {`
+<Dropdown triggerElement={() => <button>Toggle Menu</button>}>
+  <div
+    style={{
+      background: "#FFF",
+      padding: 10,
+      border: "1px solid #F1F1F1",
+      boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)"
+    }}
+  >
+    I am Dropdown Content
+  </div>
+</Dropdown>
+          `}
+          </Code>
+        </Paragraph>
+      </section>
+
+      <section>
+        <SecondaryTitle>Props</SecondaryTitle>
+
         <table className="table">
           <thead>
             <tr>
-              <th>Title 1</th>
-              <th>Title 2</th>
-              <th>Title 3</th>
-              <th>Title 4</th>
+              <th>Props</th>
+              <th>Type</th>
+              <th>Description</th>
+              <th>Default</th>
             </tr>
           </thead>
 
           <tbody>
             <tr>
-              <td>Desc one</td>
-              <td>Desc Two</td>
-              <td>Desc Three</td>
-              <td>Desc Four</td>
+              <td>children</td>
+              <td>ReactNode</td>
+              <td>React Node which will be the dropdown content</td>
+              <td>-</td>
             </tr>
             <tr>
-              <td>Desc one</td>
-              <td>Desc Two</td>
-              <td>Desc Three</td>
-              <td>Desc Four</td>
+              <td>triggerElement</td>
+              <td>{"function({ active }) => ReactNode"}</td>
+              <td>
+                Function which should return the element which will trigger the
+                dropdown
+              </td>
+              <td>-</td>
             </tr>
             <tr>
-              <td>Desc one</td>
-              <td>Desc Two</td>
-              <td>Desc Three</td>
-              <td>Desc Four</td>
+              <td>active(optional)</td>
+              <td>boolean</td>
+              <td>
+                Sets default state of dropdown, either it is active or not by
+                default
+              </td>
+              <td>false</td>
+            </tr>
+            <tr>
+              <td>isAnimated(optional)</td>
+              <td>boolean</td>
+              <td>Should animate or not while toggling between dropdown</td>
+              <td>true</td>
+            </tr>
+            <tr>
+              <td>animationType(optional)</td>
+              <td>"fade" | "expand"</td>
+              <td>Type of animation for dropdown</td>
+              <td>"expand"</td>
+            </tr>
+            <tr>
+              <td>dropdownStyles(optional)</td>
+              <td>style</td>
+              <td>Style object to style the dropdown</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>placement(optional)</td>
+              <td>
+                "bottomleft" | "bottommiddle" | "bottomright" | "topleft" |
+                "topmiddle" | "topright"
+              </td>
+              <td>Defines the placement of dropdown</td>
+              <td>"bottomright"</td>
+            </tr>
+            <tr>
+              <td>dismissOnOutsideClick(optional)</td>
+              <td>boolean</td>
+              <td>Should dismiss dropdown if we click outside dropdown</td>
+              <td>true</td>
+            </tr>
+            <tr>
+              <td>dismissOnInsideClick(optional)</td>
+              <td>boolean</td>
+              <td>
+                Should dismiss dropdown if we click on content inside dropdown
+              </td>
+              <td>false</td>
+            </tr>
+            <tr>
+              <td>toggleOnTriggerElementClick(optional)</td>
+              <td>boolean</td>
+              <td>Should toggle the dropdown if we click trigger element</td>
+              <td>false</td>
             </tr>
           </tbody>
         </table>

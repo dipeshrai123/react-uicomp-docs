3cda24523cb3cdd03c2b992d68694e8c3626565c
raidipesh78 <raidipesh78@gmail.com>
2020-08-20 11:50:04 +0545

    DropdownMenu component docs added

--- src/components/dropdownMenuPage/DropdownMenuPage.js
+++ src/components/dropdownMenuPage/DropdownMenuPage.js
@@ -19,10 +19,7 @@ const DropdownMenuPage = () => {
     <div>
       <Title>Dropdown Menu</Title>
       <section>
-        <Paragraph>
-          UI component for Dropdown Element with default styling provided by
-          react-uicomp.
-        </Paragraph>
+        <Paragraph>UI component for Dropdown Element</Paragraph>
       </section>
 
       <section>
@@ -42,6 +39,133 @@ const DropdownMenuPage = () => {
             </DropdownMenuItem>
           </DropdownMenu>
         </Dropdown>
+      </section>
+
+      <section>
+        <SecondaryTitle>DropdownMenu</SecondaryTitle>
+
+        <Paragraph>
+          Wrapper for wrapping all the dropdown items for default styling
+          provided by <span className="highlight">react-uicomp</span>
+        </Paragraph>
+      </section>
+
+      <section>
+        <SecondaryTitle>Props</SecondaryTitle>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Props</th>
+              <th>Type</th>
+              <th>Description</th>
+              <th>Default</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>children</td>
+              <td>ReactNode</td>
+              <td>Should contain list of dropdown items</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>style</td>
+              <td>style</td>
+              <td>Used to override default styling</td>
+              <td>-</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section>
+        <SecondaryTitle>DropdownMenuItem</SecondaryTitle>
+
+        <Paragraph>It defines the menu items for DropdownMenu.</Paragraph>
+      </section>
+
+      <section>
+        <SecondaryTitle>Props</SecondaryTitle>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Props</th>
+              <th>Type</th>
+              <th>Description</th>
+              <th>Default</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>children</td>
+              <td>ReactNode</td>
+              <td>Can contain any element node or text</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>style</td>
+              <td>style</td>
+              <td>Used to override default styling</td>
+              <td>-</td>
+            </tr>
+            <tr>
+              <td>danger(optional)</td>
+              <td>boolean</td>
+              <td>Highlights the item with default color</td>
+              <td>false</td>
+            </tr>
+            <tr>
+              <td>onClick</td>
+              <td>function</td>
+              <td>Handles onClick event for an item</td>
+              <td>-</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+
+      <section>
+        <SecondaryTitle>DropdownMenuSeparator</SecondaryTitle>
+
+        <Paragraph>
+          It provides some default margin and padding to top and bottom with
+          default border styling.
+        </Paragraph>
+      </section>
+
+      <section>
+        <SecondaryTitle>Example</SecondaryTitle>
+
+        <Paragraph>
+          To achieve dropdown functionality use should wrap{" "}
+          <span className="highlight">DropdownMenu</span> component with{" "}
+          <span className="highlight">Dropdown</span> component.
+          <Code>
+            {`
+import React from "react";
+import { Dropdown, DropdownMenuItem, DropdownMenuSeparator } from "react-uicomp";
+
+export default function DropdownComponent() {
+  return (
+    <Dropdown triggerElement={() => <button>Toggle Menu</button>}>
+      <DropdownMenu>
+        <DropdownMenuItem onClick={() => false}>Item 1</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => false}>Item 2</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => false} danger={true}>
+          Item 3
+        </DropdownMenuItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+}
+          `}
+          </Code>
+        </Paragraph>
       </section>
     </div>
   );

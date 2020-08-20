4a6ef982d6de130f2c6aba9df70848d4bc580d0d
raidipesh78 <raidipesh78@gmail.com>
2020-08-20 13:38:19 +0545

    Example changed

--- src/components/demos/toast/index.js
+++ src/components/demos/toast/index.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { loremIpsum } from "lorem-ipsum";
 import { Toast, useToast } from "react-uicomp";
 
 const ToastComponent = () => {
+  const [mod, setMod] = useState(false);
   const { handler, toast } = useToast();
 
   return (
     <div>
       <button
         className="button"
-        onClick={() => toast({ message: loremIpsum(), type: "success" })}
+        onClick={() => {
+          setMod((prev) => !prev);
+          toast({ message: loremIpsum(), type: mod ? "success" : "error" });
+        }}
       >
         Open Toast
       </button>

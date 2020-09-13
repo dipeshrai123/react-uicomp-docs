a982a880f33f26d221234cae5679c1e0e5c289e4
raidipesh78 <raidipesh78@gmail.com>
2020-09-13 11:38:37 +0545

    Animated side nav

--- src/components/common/sidenav/components/NavGroup.component.js
+++ src/components/common/sidenav/components/NavGroup.component.js
@@ -1,43 +1,45 @@
-import React from "react";
-import { ActiveLink } from "react-uicomp";
+import React, { useState } from "react";
+import {
+  ActiveLink,
+  useMeasure,
+  useAnimatedValue,
+  AnimatedBlock,
+} from "react-uicomp";
 
 const NavGroup = ({ title, navItems }) => {
-  // const { handler, height } = useMeasure();
-  // const [expanded, setExpanded] = useState(true);
-  // const animatedHeight = useAnimatedValue(expanded ? height : 0);
+  const { handler, height } = useMeasure();
+  const [expanded, setExpanded] = useState(true);
+  const animatedHeight = useAnimatedValue(expanded ? height : 0);
 
   return (
     <div className="sidenav__nav-group">
-      <h3
-        className="sidenav__title"
-        // onClick={() => setExpanded(!expanded)}
-      >
+      <h3 className="sidenav__title" onClick={() => setExpanded(!expanded)}>
         {title}
       </h3>
-      {/* <AnimatedBlock
+      <AnimatedBlock
         style={{
           height: animatedHeight.value,
           overflow: "hidden",
         }}
-      > */}
-      <ul
-        // {...handler}
-
-        className="sidenav__navitems"
       >
-        {navItems.map((item, index) => (
-          <li key={index} className="sidenav__navitem">
-            <ActiveLink
-              to={item.link}
-              className="sidenav__navlink"
-              activeClassName="sidenav__navlink--active"
-            >
-              {item.name}
-            </ActiveLink>
-          </li>
-        ))}
-      </ul>
-      {/* </AnimatedBlock> */}
+        <ul
+          {...handler}
+          className="sidenav__navitems"
+          style={{ paddingBottom: 5 }}
+        >
+          {navItems.map((item, index) => (
+            <li key={index} className="sidenav__navitem">
+              <ActiveLink
+                to={item.link}
+                className="sidenav__navlink"
+                activeClassName="sidenav__navlink--active"
+              >
+                {item.name}
+              </ActiveLink>
+            </li>
+          ))}
+        </ul>
+      </AnimatedBlock>
     </div>
   );
 };
